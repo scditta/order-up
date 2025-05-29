e28d80dbbf4c160c55b824317075f878505ff909
Stephen <sdittawork@gmail.com>
2025-05-28 20:56:14 -0400

    modified game data to be utilized in the dashboard component and shared in the children components setup and new game

--- orderup-game/src/app/dashboard/page.tsx
+++ orderup-game/src/app/dashboard/page.tsx
@@ -10,12 +10,26 @@ import NewGame from "../new-game/page";
 export default function Page() {
   const [beginGame, setBeginGame] = useState<boolean>(false);
 
+  const [gameData, setGameData] = useState<{
+    listSize: number;
+    categoryID: number;
+    difficulty: string;
+  }>({
+    listSize: 5,
+    categoryID: 1,
+    difficulty: "easy",
+  });
+
   return (
     <div className="h-screen w-screen">
       {beginGame ? (
         <NewGame setGame={setBeginGame} />
       ) : (
-        <Setup setGame={setBeginGame} />
+        <Setup
+          setGame={setBeginGame}
+          gameData={gameData}
+          setGameData={setGameData}
+        />
       )}
     </div>
   );
--- orderup-game/src/app/setup/page.tsx
+++ orderup-game/src/app/setup/page.tsx
@@ -3,37 +3,52 @@
 import Link from "next/link";
 import { useState } from "react";
 
-const categories = [
-  { name: "Anything", id: 1 },
-  { name: "History", id: 2 },
-  { name: "Science", id: 3 },
-  { name: "Sports", id: 4 },
-  { name: "Entertainment", id: 5 },
-  { name: "Art", id: 6 },
-  { name: "Geography", id: 7 },
-  { name: "Music", id: 8 },
-  { name: "Movies", id: 9 },
-  { name: "Literature", id: 10 },
-  { name: "Technology", id: 11 },
-  { name: "Nature", id: 12 },
-  { name: "Animals", id: 13 },
-  { name: "Mythology", id: 14 },
-  { name: "Languages", id: 15 },
-  { name: "Philosophy", id: 16 },
-  { name: "Psychology", id: 17 },
-  { name: "Religion", id: 18 },
-  { name: "Politics", id: 19 },
-  { name: "Business", id: 20 },
-  { name: "Food", id: 21 },
-];
+import { categories } from "@/app/catagories.json";
 
-export default function Setup({
-  setGame,
-}: {
+// const categories = [
+//   { name: "Anything", id: 1 },
+//   { name: "History", id: 2 },
+//   { name: "Science", id: 3 },
+//   { name: "Sports", id: 4 },
+//   { name: "Entertainment", id: 5 },
+//   { name: "Art", id: 6 },
+//   { name: "Geography", id: 7 },
+//   { name: "Music", id: 8 },
+//   { name: "Movies", id: 9 },
+//   { name: "Literature", id: 10 },
+//   { name: "Technology", id: 11 },
+//   { name: "Nature", id: 12 },
+//   { name: "Animals", id: 13 },
+//   { name: "Mythology", id: 14 },
+//   { name: "Languages", id: 15 },
+//   { name: "Philosophy", id: 16 },
+//   { name: "Psychology", id: 17 },
+//   { name: "Religion", id: 18 },
+//   { name: "Politics", id: 19 },
+//   { name: "Business", id: 20 },
+//   { name: "Food", id: 21 },
+// ];
+
+interface SetupProps {
   setGame: (value: boolean) => void;
-}) {
-  const [numberOfResults, setNumberOfResults] = useState<number>(5);
-  const [category, setcategory] = useState<string>("anything");
+  gameData: {
+    listSize: number;
+    categoryID: number;
+    difficulty: string;
+  };
+  setGameData: React.Dispatch<
+    React.SetStateAction<{
+      listSize: number;
+      categoryID: number;
+      difficulty: string;
+    }>
+  >;
+}
+
+export default function Setup({ setGame, gameData, setGameData }: SetupProps) {
+  // const [listSize, setListSize] = useState<number>(5);
+  // const [categoryID, setcategoryID] = useState<number>(1);
+  // const [difficulty, setDifficulty] = useState<string>("easy"); // easy, medium, hard
 
   const updateSetSize = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = parseInt(e.target.value);
@@ -47,7 +62,8 @@ export default function Setup({
       alert("Please enter a valid number");
       return;
     }
-    setNumberOfResults(value);
+    setGameData((prev) => ({ ...prev, listSize: value }));
+    // setListSize(value);
   };
 
   return (
@@ -59,10 +75,26 @@ export default function Setup({
             <div>Categories</div>
             <div className="grid grid-cols-3 gap-5 mx-5">
               {categories.slice(0, 8).map((category, index) => {
+                if (gameData.categoryID === category.id) {
+                  return (
+                    <div
+                      key={index}
+                      className="bg-amber-700 p-2 cursor-pointer hover:bg-amber-400"
+                    >
+                      {category.name}
+                    </div>
+                  );
+                }
                 return (
                   <div
                     key={index}
                     className="bg-amber-500 p-2 cursor-pointer hover:bg-amber-400"
+                    onClick={() => {
+                      setGameData((prev) => ({
+                        ...prev,
+                        categoryID: category.id,
+                      }));
+                    }}
                   >
                     {category.name}
                   </div>
@@ -78,9 +110,35 @@ export default function Setup({
             <input
               type="number"
               className="m-2 px-2 w-14 bg-white border-1 rounded-sm"
-              value={numberOfResults}
+              value={gameData.listSize}
               onChange={updateSetSize}
             />
+          </div>
+          <div>
+            <p>Difficulty</p>
+            <select
+              name="difficulty"
+              id="difficulty"
+              className="m-2 px-2 w-30 bg-white border-1 rounded-sm"
+              value={gameData.difficulty}
+              onChange={(e) => {
+                setGameData((prev) => ({
+                  ...prev,
+                  difficulty: e.target.value,
+                }));
+              }}
+            >
+              <option value="easy">Easy</option>
+              <option value="medium">Medium</option>
+              <option value="hard">Hard</option>
+              <option value="advance">Advance</option>
+            </select>
+            {/* <input
+              type=""
+              className="m-2 px-2 w-14 bg-white border-1 rounded-sm"
+              value={gameData.listSize}
+              onChange={updateSetSize}
+            /> */}
           </div>
           <button
             type="button"
